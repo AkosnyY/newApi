a78b8d7f37abbde55b9778de5521acfd7dfa72bc
akosy <develllopeurweb@gmail.com>
2023-03-16 10:42:41 +0100

    AJout du fichier index.js de la newApi

---
+++ index.js
@@ -0,0 +1,9 @@
+// Importer le module express
+
+//Créer l'instance express 
+
+// Définir le port et le stocker 
+
+// Créer une route git sur la racine de votre application web 
+
+// Lancer le server 
